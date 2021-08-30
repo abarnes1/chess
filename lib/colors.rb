4df1f0540c6d37d82ae123ab020b096e8dfb0296
Aaron Barnes <aaron.barnes1@hotmail.com>
2021-08-30 18:54:55 -0500

    create colors class
    - Colors class contains color constants that will be used to style chess
      squares and they pieces they contain.

---
+++ lib/colors.rb
@@ -0,0 +1,23 @@
+class Colors
+  self::BLACK = '30'.freeze
+  self::RED = '31'.freeze
+  self::GREEN = '32'.freeze
+  self::BROWN = '33'.freeze
+  self::BLUE = '34'.freeze
+  self::MAGENTA = '35'.freeze
+  self::CYAN = '36'.freeze
+  self::GRAY = '37'.freeze
+
+  self::BG_BLACK = '40'.freeze
+  self::BG_RED = '41'.freeze
+  self::BG_GREEN = '42'.freeze
+  self::BG_BROWN = '43'.freeze
+  self::BG_BLUE = '44'.freeze
+  self::BG_MAGENTA = '45'.freeze
+  self::BG_CYAN = '46'.freeze
+  self::BG_GRAY = '47'.freeze
+
+  # https://chrisyeh96.github.io/2020/03/28/terminal-colors.html
+  # 90-97	bright foreground color (non-standard)
+  # 100-107	bright background color (non-standard)
+end
