77e044527be8645b4093dc98b9bd8f1625f6e043
Aaron Barnes <aaron.barnes1@hotmail.com>
2021-08-30 19:23:39 -0500

    add Pieces class
    - Pieces class contains constants to be able to place string
      representations of chess pieces into chess squares for movement
      testing.

---
+++ lib/pieces.rb
@@ -0,0 +1,15 @@
+class Pieces
+  self::KING_SOLID = "\u265A".freeze
+  self::QUEEN_SOLID = "\u265B".freeze
+  self::ROOK_SOLID = "\u265C".freeze
+  self::BISHOP_SOLID = "\u265D".freeze
+  self::KNIGHT_SOLID = "\u265E".freeze
+  self::PAWN_SOLID = "\u265F".freeze
+
+  self::KING_OUTLINE = "\u2654".freeze
+  self::QUEEN_OUTLINE = "\u2655".freeze
+  self::ROOK_OUTLINE = "\u2656".freeze
+  self::BISHOP_OUTLINE = "\u2657".freeze
+  self::KNIGHT_OUTLINE = "\u2658".freeze
+  self::PAWN_OUTLINE = "\u2659".freeze
+end
