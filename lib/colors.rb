3984e579ce0e2ed3511668680f7849a80d3edfc6
Aaron Barnes <aaron.barnes1@hotmail.com>
2021-08-30 20:04:15 -0500

    add non-standard bright variants to Colors class

--- lib/colors.rb
+++ lib/colors.rb
@@ -20,4 +20,22 @@ class Colors
   # https://chrisyeh96.github.io/2020/03/28/terminal-colors.html
   # 90-97	bright foreground color (non-standard)
   # 100-107	bright background color (non-standard)
+
+  self::BRIGHT_BLACK = '90'.freeze
+  self::BRIGHT_RED = '91'.freeze
+  self::BRIGHT_GREEN = '92'.freeze
+  self::BRIGHT_BROWN = '93'.freeze
+  self::BRIGHT_BLUE = '94'.freeze
+  self::BRIGHT_MAGENTA = '95'.freeze
+  self::BRIGHT_CYAN = '96'.freeze
+  self::BRIGHT_GRAY = '97'.freeze
+
+  self::BG_BRIGHT_BLACK = '100'.freeze
+  self::BG_BRIGHT_RED = '101'.freeze
+  self::BG_BRIGHT_GREEN = '102'.freeze
+  self::BG_BRIGHT_BROWN = '103'.freeze
+  self::BG_BRIGHT_BLUE = '104'.freeze
+  self::BG_BRIGHT_MAGENTA = '105'.freeze
+  self::BG_BRIGHT_CYAN = '106'.freeze
+  self::BG_BRIGHT_GRAY = '107'.freeze
 end
