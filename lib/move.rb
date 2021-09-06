facb260feeaf7f4a37f291af8357cd067b97e138
Aaron Barnes <aaron.barnes1@hotmail.com>
2021-09-06 10:43:00 -0500

    add Move class
    - add lib/move.rb to represent a chess move
      - contains a starting position
      - contains a sequence of moves that can represent
        a move to a single square or a linear sequence
      - able to stop a sequence at a particular position in
        a sequence

---
+++ lib/move.rb
@@ -0,0 +1,28 @@
+# Linked-list type behavior to act like single move (knight) or single move sequence (bishop, rook) that can be stopped at any node
+class Move
+  attr_accessor :sequence
+  attr_reader :start_position
+
+  def initialize(start_position, sequence = [])
+    @start_position = start_position
+    @sequence = sequence
+  end
+
+  def stop_at(stop_position)
+    stop_index = @sequence.find_index { |position| position == stop_position }
+
+    @sequence = @sequence[0..stop_index] unless stop_index.nil?
+  end
+
+  def to_s
+    output = start_position.position
+
+    sequence.each { |pos| output += " -> #{pos.position}"}
+
+    output
+  end
+
+  def contains?(position)
+    sequence.contains?(position)
+  end
+end
