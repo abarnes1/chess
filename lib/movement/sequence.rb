6823bd87aea1e30b78b9b72cf6998d1c0476c2a9
Aaron Barnes <aaron.barnes1@hotmail.com>
2021-09-12 18:05:56 -0500

    remove bad ideas that don't work
    - delete PositionHelpers module
    - delete PositionHelpers tests
    - delete Sequence class
    - delete Sequence class tests

--- lib/movement/sequence.rb
+++ lib/movement/sequence.rb
@@ -1,8 +1,10 @@
 require_relative '../position_helpers'
+require_relative 'offset'
 
 # A series of coordinate offsets used to calculate position
-# to position movements for chess pieces.
-class Sequence
+# to position movements for chess pieces.  Each offset coordinate
+# is treated like an independent move and returned if valid.
+class PositionSequence
   include PositionHelpers
 
   attr_reader :offsets, :start_position
@@ -19,13 +21,36 @@ class Sequence
     next_start_position = start_position
 
     offsets.each do |offset|
-      new_position = calculate_position(next_start_position, offset)
+      new_position = calculate_position(start_position, offset)
       break if new_position.nil?
 
       next_start_position = new_position
-      positions << new_position
+      positions << new_position if valid_new_position?(new_position, positions)
     end
 
     positions
   end
+
+  private
+
+  def valid_new_position?(new_position, positions)
+    if new_position == start_position || positions.include?(new_position)
+      false
+    else
+      true
+    end
+  end
 end
+
+# test = Sequence.new(Position.new('c4'), [[1, 1], [-1, -1]])
+# test = Sequence.new(Position.new('c4'), [[0, 1], [0, 2]])
+# puts test.positions
+
+offset_groups = [
+  Offset.new([1, 1]),
+  Offset.new([-1, -1])
+]
+
+test = Sequence.new(Position.new('c4'), offset_groups)
+puts test.positions
+
