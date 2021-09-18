a3960821cf3abf58f4a7c6f995c45ae1e1fee4bb
Aaron Barnes <aaron.barnes1@hotmail.com>
2021-09-18 17:54:43 -0500

    default notation letter to icon unless supplied

--- lib/pieces/chesspiece.rb
+++ lib/pieces/chesspiece.rb
@@ -3,13 +3,13 @@ require_relative '../movement/offset'
 require_relative '../movement/repeat_offset'
 
 class ChessPiece
-  attr_reader :position, :algebraic_letter, :owner
+  attr_reader :position, :notation_letter, :owner
 
-  def initialize(icon: 'X', owner: nil, position: nil, offsets: nil)
+  def initialize(icon: 'X', owner: nil, position: nil, offsets: nil, notation_letter: nil)
     @icon = icon.freeze
     @position = position
     @offsets = offsets
-    @notation_letter = 'X'
+    @notation_letter = notation_letter.nil? ? icon : notation_letter
     @owner = owner
   end
 
