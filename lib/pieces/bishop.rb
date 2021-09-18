f624fa939150de5aa408070fa8c901489abbfee0
Aaron Barnes <aaron.barnes1@hotmail.com>
2021-09-18 17:55:43 -0500

    remove unused functions

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -1,41 +1,21 @@
 # frozen_string_literal: true
 
 require_relative 'chesspiece'
-require_relative '../position_helpers'
+require_relative '../modules/positioning'
 
 class Bishop < ChessPiece
-  include PositionHelpers
+  include Positioning
 
-  def initialize(icon: "\u265D", position: nil)
-    super(icon: icon, position: position)
-    @movements = [[-1, -1], [-1, 1], [1, -1], [1, 1]]
-    @notation_letter = 'B'.freeze
-  end
+  def initialize(icon: "\u265D", position: nil, owner: nil)
+    super(icon: icon, position: position, owner: owner)
 
-  def possible_moves
-    moves = []
+    @offsets = [
+      RepeatOffset.new([-1, -1]),
+      RepeatOffset.new([-1,  1]),
+      RepeatOffset.new([1, -1]),
+      RepeatOffset.new([1,  1])
+    ]
 
-    @movements.each do |offset|
-      new_move = Move.new(position)
-
-      new_move.sequence = generate_sequence(new_move, offset)
-      moves << new_move unless new_move.sequence.empty?
-    end
-
-    moves
-  end
-
-  private
-
-  def generate_sequence(move, offset)
-    sequence = []
-    next_position = calculate_position(move.start_position, offset)
-
-    while inbounds?(next_position)
-      sequence << next_position
-      next_position = calculate_position(next_position, offset)
-    end
-
-    sequence
+    @notation_letter = 'B'
   end
 end
