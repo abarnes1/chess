f86544fe5989114a06e296106741807cdcbc6199
Aaron Barnes <aaron.barnes1@hotmail.com>
2021-09-25 22:22:12 -0500

    remove debug output and functions moved to GameState

--- lib/pieces/piece_collection.rb
+++ lib/pieces/piece_collection.rb
@@ -3,6 +3,7 @@
 require_relative 'chesspiece'
 require_relative '../movement_helpers'
 
+# A set of chess pieces, both friend and foe.
 class PieceCollection
   attr_reader :pieces
 
@@ -27,34 +28,22 @@ class PieceCollection
 
     return false if piece.nil?
 
-    result = piece.owner != friendly_owner
-    puts "occupied_by_enemy? (#{position}): #{result}"
-    result
+    piece.owner != friendly_owner
   end
 
   def friendly_at?(friendly_owner, position)
+    return false if position.nil?
+
     piece = select_by_position(position)
     return false if piece.nil?
-    
-    result = piece.owner == friendly_owner
-    puts "occupied_by_friendly? (#{position}): #{result}"
-    result
+
+    piece.owner == friendly_owner
   end
 
   def occupied_at?(position)
     piece = select_by_position(position)
 
     piece.nil? ? false : true
-  end
-
-  # can single square be attacked by any of my pieces
-  def attackable?(position)
-    raise NotImplementedError
-  end
-
-  # get all pieces that can attack a square
-  def pieces_that_attack(position)
-    raise NotImplementedError
   end
 
   def select_by_position(position)
