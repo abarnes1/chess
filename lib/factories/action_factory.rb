10462a9def234fca86a35d56cf4b176664c3e6ab
Aaron Barnes <aaron.barnes1@hotmail.com>
2021-09-25 22:16:51 -0500

    rework ActionFactory
    - now creates all actions for Action child classes
    - works for Move and Capture types currently

--- lib/factories/action_factory.rb
+++ lib/factories/action_factory.rb
@@ -2,12 +2,26 @@
 
 Dir['lib/actions/*.rb'].sort.each { |file| require_relative "../../#{file}" }
 
+# Creates all actions of Action class children types given a Chesspiece
+# and GameState object.
 class ActionFactory
   def initialize; end
 
   def self.create_action(action_type, *args)
-    # puts "MoveFactory for type: #{action_type}"
-
     action_type.new(*args)
+  end
+
+  def self.actions_for(piece, game_state)
+    moves = []
+
+    Action.registered_children.each do |action_type|
+      next unless [Move, Capture].include? action_type
+
+      # puts "creating moves for child type: #{action_type}"
+      moves << action_type.create_for(piece, game_state)
+    end
+
+    # convert array of different move type arrays to single array
+    moves.flatten
   end
 end
