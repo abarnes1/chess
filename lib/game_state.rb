861afdaedc89aff7b1d2b30f6b49a6d39416c534
Aaron Barnes <aaron.barnes1@hotmail.com>
2021-09-26 19:32:02 -0500

    add GameState object
    - wraps action log and piece collection together for simpler
      implementation of querying the status of the game
    - a PieceCollection and action log are needed together to answer
      many questions about the state of the game
    - hides some lower level nil checking of PieceCollection

---
+++ lib/game_state.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+# Wrapper for the state of a game of Chess that can answer
+# questions necessary to generate piece actions.  Exists to
+# simplify questions about aspects of the game state for action
+# creation without adding a bunch of methods to the Action class(es).
+class GameState
+  attr_reader :piece_collection, :action_log
+
+  def initialize(piece_collection = nil, action_log = [])
+    @piece_collection = piece_collection
+    @action_log = action_log
+  end
+
+  def clone
+    Marshal.load(Marshal.dump(self))
+  end
+
+  def promote?(piece, position)
+    piece.respond_to?(:promote_at?) && piece.promote_at?(position)
+  end
+
+  def blocked_at?(position)
+    return false if piece_collection.nil?
+
+    piece_collection.occupied_at?(position)
+  end
+
+  def blocked_by_friendly?(owner, position)
+    return false if piece_collection.nil?
+
+    piece_collection.friendly_at?(owner, position)
+  end
+
+  def enemy_at?(owner, position)
+    return false if piece_collection.nil?
+
+    piece_collection.enemy_at?(owner, position)
+  end
+
+  def select_by_position(position)
+    piece_collection.select_by_position(position)
+  end
+
+  def add_piece(piece)
+    piece_collection.add(piece)
+  end
+
+  def remove_piece(piece)
+    piece_collection.remove(piece)
+  end
+
+  def log_action(action)
+    @action_log << action
+  end
+
+  def last_moves(count)
+    to_display = action_log.reverse[0..count]
+    to_display.join("\n")
+  end
+
+  # can single square be attacked by any of my pieces
+  def attackable?(position)
+    raise NotImplementedError
+  end
+
+  # get all pieces that can attack a square
+  def pieces_that_attack(position)
+    raise NotImplementedError
+  end
+end
