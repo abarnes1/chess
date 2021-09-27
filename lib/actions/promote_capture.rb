e89b83903856b8a39806facaf61a53a19b58d7b8
Aaron Barnes <aaron.barnes1@hotmail.com>
2021-09-26 19:22:13 -0500

    add PromoteCapture action type
    - represents a chess action where a piece captures another and
      can also promote at the destination square

---
+++ lib/actions/promote_capture.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+# A basic chess move where one piece moves from its current
+# square to another square occupied by an enemy piece.
+class PromoteCapture < Action
+  attr_accessor :promote_to
+  attr_reader :move_to, :piece
+
+  def initialize(piece, move_from, move_to, captured, promote_to = '?')
+    super
+    @piece = piece
+    @promote_to = promote_to
+    @move_to = move_to
+
+    puts "initializing #{self}"
+  end
+
+  def self.create_for(piece, game_state)
+    moves = []
+
+    sequences = calculate_sequences(piece.position, piece.capture_offsets)
+
+    sequences.each do |sequence|
+      capturable_piece = first_capturable_piece(piece, game_state, sequence)
+
+      next if capturable_piece.nil?
+
+      capture_promote = new(piece, piece.position, capturable_piece.position, capturable_piece)
+      moves << capture_promote
+    end
+
+    moves
+  end
+
+  def apply(game_state)
+    game_state.log_action(self)
+
+    game_state.remove_piece(piece)
+    game_state.add_piece(promote_to)
+
+    promote_to.position = move_to
+  end
+
+  def to_s
+    "promote capture: #{@piece} to #{@promote_to} at #{@move_to}"
+  end
+
+  def notation
+    "#{@move_to}xe#{@promote_to}"
+  end
+
+  def self.first_capturable_piece(piece, game_state, sequence)
+    sequence.each do |position|
+      break if game_state.blocked_by_friendly?(piece.owner, position)
+
+      next unless game_state.enemy_at?(piece.owner, position)
+
+      capture_piece = game_state.select_by_position(position)
+      return capture_piece if game_state.promote?(piece, position)
+    end
+
+    nil
+  end
+end
