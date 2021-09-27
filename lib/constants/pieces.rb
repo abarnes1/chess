ed9dd3a26ce88e3e40eedc16fa092bab06b7e46a
Aaron Barnes <aaron.barnes1@hotmail.com>
2021-09-27 13:46:41 -0500

    add global freeze comment

--- lib/constants/pieces.rb
+++ lib/constants/pieces.rb
@@ -1,15 +1,18 @@
-class Pieces
-  self::KING_SOLID = "\u265A".freeze
-  self::QUEEN_SOLID = "\u265B".freeze
-  self::ROOK_SOLID = "\u265C".freeze
-  self::BISHOP_SOLID = "\u265D".freeze
-  self::KNIGHT_SOLID = "\u265E".freeze
-  self::PAWN_SOLID = "\u265F".freeze
+# frozen_string_literal: true
 
-  self::KING_OUTLINE = "\u2654".freeze
-  self::QUEEN_OUTLINE = "\u2655".freeze
-  self::ROOK_OUTLINE = "\u2656".freeze
-  self::BISHOP_OUTLINE = "\u2657".freeze
-  self::KNIGHT_OUTLINE = "\u2658".freeze
-  self::PAWN_OUTLINE = "\u2659".freeze
+# Unicode characters for chess pieces.
+class Pieces
+  self::KING_SOLID = "\u265A"
+  self::QUEEN_SOLID = "\u265B"
+  self::ROOK_SOLID = "\u265C"
+  self::BISHOP_SOLID = "\u265D"
+  self::KNIGHT_SOLID = "\u265E"
+  self::PAWN_SOLID = "\u265F"
+
+  self::KING_OUTLINE = "\u2654"
+  self::QUEEN_OUTLINE = "\u2655"
+  self::ROOK_OUTLINE = "\u2656"
+  self::BISHOP_OUTLINE = "\u2657"
+  self::KNIGHT_OUTLINE = "\u2658"
+  self::PAWN_OUTLINE = "\u2659"
 end
