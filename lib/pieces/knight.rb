6e8c12695ab2f2bf4edb5551fd235073d7bb9957
Aaron Barnes <aaron.barnes1@hotmail.com>
2021-10-03 16:47:08 -0500

    add Knight piece

---
+++ lib/pieces/knight.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require_relative 'chesspiece'
+
+# Standard knight for a game of chess.
+class Knight < ChessPiece
+  def initialize(icon: "\u265E", position: nil, owner: nil)
+    super(icon: icon, position: position, owner: owner)
+
+    @offsets = [
+      Offset.new([2, -1]),
+      Offset.new([2,  1]),
+      Offset.new([1, -2]),
+      Offset.new([-1, -2]),
+      Offset.new([-2, 1]),
+      Offset.new([-2, -1]),
+      Offset.new([-1, 2]),
+      Offset.new([1, 2])
+    ]
+
+    @notation_letter = 'K'
+  end
+end
