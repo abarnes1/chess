6a8e05a7f8cb2721a3899c9e772a400d103d437e
Aaron Barnes <aaron.barnes1@hotmail.com>
2021-10-05 21:07:15 -0500

    remove unnecessary tests for #== when compared to nil

--- spec/movement/offset_spec.rb
+++ spec/movement/offset_spec.rb
@@ -2,51 +2,46 @@ require_relative '../../lib/movement/offset'
 
 describe Offset do
   context '#initialize' do
-    let(:x) { 0 }
-    let(:y) { 1 }
-    subject(:new_offset) { described_class.new([x, y])}
+    x_coord = 0
+    y_coord = 1
+    subject(:new_offset) { described_class.new([x_coord, y_coord])}
 
     it 'sets the x value with first array element' do
       actual = new_offset.x
-      expect(actual).to eq(x)
+      expect(actual).to eq(x_coord)
     end
 
     it 'sets the y value with second array element' do
       actual = new_offset.y
-      expect(actual).to eq(y)
+      expect(actual).to eq(y_coord)
     end
   end
 
   context '#==' do
-    let(:x) { 0 }
-    let(:y) { 1 }
-    subject(:first_offset) { described_class.new([x, y])}
+    x_coord = 0
+    y_coord = 0
+    subject(:first_offset) { described_class.new([x_coord, y_coord])}
     let(:second_offset) { double(Offset) }
 
     it 'is not equal when x does not match' do
-      allow(second_offset).to receive(:x).and_return(x + 1)
-      allow(second_offset).to receive(:y).and_return(y)
+      allow(second_offset).to receive(:x).and_return(x_coord + 1)
+      allow(second_offset).to receive(:y).and_return(y_coord)
 
       actual = (first_offset == second_offset)
       expect(actual).to be false
     end
 
     it 'is not equal when y does not match' do
-      allow(second_offset).to receive(:x).and_return(x)
-      allow(second_offset).to receive(:y).and_return(y + 1)
+      allow(second_offset).to receive(:x).and_return(x_coord)
+      allow(second_offset).to receive(:y).and_return(y_coord + 1)
 
       actual = (first_offset == second_offset)
       expect(actual).to be false
     end
 
-    it 'is not equal compared to nil' do
-      actual = (first_offset == nil)
-      expect(actual).to be false
-    end
-
     it 'is equal when x and y match' do
-      allow(second_offset).to receive(:x).and_return(x)
-      allow(second_offset).to receive(:y).and_return(y)
+      allow(second_offset).to receive(:x).and_return(x_coord)
+      allow(second_offset).to receive(:y).and_return(y_coord)
 
       actual = (first_offset == second_offset)
       expect(actual).to be true
--- spec/movement/repeat_offset_spec.rb
+++ spec/movement/repeat_offset_spec.rb
@@ -2,19 +2,19 @@ require_relative '../../lib/movement/repeat_offset'
 
 describe RepeatOffset do
   context '#initialize' do
-    let(:x) { 0 }
-    let(:y) { 1 }
-    let(:max_repeats) { 3 }
-    subject(:new_offset) { described_class.new([x, y], max_repeats)}
+    x_coord = 0
+    y_coord = 0
+    max_repeats = 3
+    subject(:new_offset) { described_class.new([x_coord, y_coord], max_repeats)}
 
     it 'sets x value with first array element' do
       actual = new_offset.x
-      expect(actual).to eq(x)
+      expect(actual).to eq(x_coord)
     end
 
     it 'sets y value with second array element' do
       actual = new_offset.y
-      expect(actual).to eq(y)
+      expect(actual).to eq(y_coord)
     end
 
     it 'sets max repeats value with second parameter' do
@@ -24,22 +24,22 @@ describe RepeatOffset do
 
     it 'returns default max repeats when not supplied' do
       default_repeats = 100
-      actual = RepeatOffset.new([x, y]).max_repeats
+      actual = RepeatOffset.new([x_coord, y_coord]).max_repeats
 
       expect(actual).to eq(default_repeats)
     end
   end
 
   context '#==' do
-    let(:x) { 0 }
-    let(:y) { 1 }
-    let(:max_repeats) { 3 }
-    subject(:first_offset) { described_class.new([x, y], max_repeats)}
+    x_coord = 0
+    y_coord = 0
+    max_repeats = 3
+    subject(:first_offset) { described_class.new([x_coord, y_coord], max_repeats)}
     let(:second_offset) { double(Offset) }
 
     it 'is not equal when x does not match' do
-      allow(second_offset).to receive(:x).and_return(x + 1)
-      allow(second_offset).to receive(:y).and_return(y)
+      allow(second_offset).to receive(:x).and_return(x_coord + 1)
+      allow(second_offset).to receive(:y).and_return(y_coord)
       allow(second_offset).to receive(:max_repeats).and_return(max_repeats)
 
       actual = (first_offset == second_offset)
@@ -47,8 +47,8 @@ describe RepeatOffset do
     end
 
     it 'is not equal when y does not match' do
-      allow(second_offset).to receive(:x).and_return(x)
-      allow(second_offset).to receive(:y).and_return(y + 1)
+      allow(second_offset).to receive(:x).and_return(x_coord)
+      allow(second_offset).to receive(:y).and_return(y_coord + 1)
       allow(second_offset).to receive(:max_repeats).and_return(max_repeats)
 
       actual = (first_offset == second_offset)
@@ -56,22 +56,17 @@ describe RepeatOffset do
     end
 
     it 'is not equal when max_repeats do not match' do
-      allow(second_offset).to receive(:x).and_return(x)
-      allow(second_offset).to receive(:y).and_return(y)
+      allow(second_offset).to receive(:x).and_return(x_coord)
+      allow(second_offset).to receive(:y).and_return(y_coord)
       allow(second_offset).to receive(:max_repeats).and_return(max_repeats + 1)
 
       actual = (first_offset == second_offset)
       expect(actual).to be false
     end
 
-    it 'is not equal compared to nil' do
-      actual = (first_offset == nil)
-      expect(actual).to be false
-    end
-
     it 'is equal when x, y, and max repeats match' do
-      allow(second_offset).to receive(:x).and_return(x)
-      allow(second_offset).to receive(:y).and_return(y)
+      allow(second_offset).to receive(:x).and_return(x_coord)
+      allow(second_offset).to receive(:y).and_return(y_coord)
       allow(second_offset).to receive(:max_repeats).and_return(3)
 
       actual = (first_offset == second_offset)
