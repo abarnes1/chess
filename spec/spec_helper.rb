907ee797ea9ae9deb89c61af50a8d920b1ff8188
Aaron Barnes <aaron.barnes1@hotmail.com>
2021-10-09 23:09:23 -0500

    add global suppression of #puts

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -98,3 +98,18 @@ RSpec.configure do |config|
   Kernel.srand config.seed
 =end
 end
+
+# disable outputs to :puts
+RSpec.configure do |config|
+  original_stderr = $stderr
+  original_stdout = $stdout
+  config.before(:all) do
+    # Redirect stderr and stdout
+    $stderr = File.open(File::NULL, "w")
+    $stdout = File.open(File::NULL, "w")
+  end
+  config.after(:all) do
+    $stderr = original_stderr
+    $stdout = original_stdout
+  end
+end
