e4e1ff95750de3b2af07c46caa01dc4a4ec04e19
Aaron Barnes <aaron.barnes1@hotmail.com>
2021-10-24 20:38:55 -0500

    continue setting up game loop

--- lib/chess_game.rb
+++ lib/chess_game.rb
@@ -25,20 +25,32 @@ class ChessGame
     # classes necessary for game to run
     @game_state = nil
     @current_player = nil
+    @winner = nil
+  end
+
+  def play_game
+    print_intro
+    setup_game
+    puts @gameboard.to_s
+
+    play_turns
+    end_game
   end
 
   def setup_game
     # create players
-    @white = create_player('white')
-    @black = create_player('black')
+    @white = create_player('white', Colors::CYAN)
+    @black = create_player('black', Colors::BRIGHT_MAGENTA)
     @current_player = @white
+
+    @game_state = GameState.standard_pieces(@white, @black)
   end
 
   def create_player(label)
     player_type = ''
 
     until %w[1 2].include?(player_type)
-      print "Choose a player type for #{name} #{token}\nEnter 1 for Human or 2 for Computer: "
+      print "Choose a player type for #{label}\nEnter 1 for Human or 2 for Computer: "
       player_type = gets.chomp
     end
 
@@ -49,6 +61,43 @@ class ChessGame
     else
       ComputerPlayer.new(label)
     end
+  end
+
+  def play_turns
+    until game_over?
+      play_next_turn(@current_player)
+      puts @gameboard.to_s
+
+      @winner = @current_player if game_won?
+
+      switch_player if @winner.nil?
+    end
+  end
+
+  def play_next_turn(player)
+    # input = player_input(player)
+    # @last_move_identifier = @gameboard.drop(input, player.token)
+  end
+
+  def player_input(player)
+    # column = nil
+
+    # until valid_move?(column)
+    #   if player.instance_of?(HumanPlayer)
+    #     print "Choose a column for #{player.name} #{player.token} : "
+    #     column = gets.chomp.to_i
+    #   elsif player.instance_of?(ComputerPlayer)
+    #     sleep(1)
+    #     column = player.choose_move(@gameboard.valid_moves)
+    #     puts "#{player.name} #{player.token}  chooses column ##{column}"
+    #   end
+    # end
+
+    # column
+  end
+
+  def game_won?
+
   end
 
   private
@@ -64,5 +113,47 @@ class ChessGame
 
   def switch_player
     @current_player = @current_player == @white ? @black : @white
+  end
+
+  def print_intro
+    puts 'Play some chess.'
+  end
+  
+  def standard_piece_setup
+    @game_state.add_piece(Rook.new(position: Position.new('a1'), owner: @white))
+    @game_state.add_piece(Knight.new(position: Position.new('b1'), owner: @white))
+    @game_state.add_piece(Bishop.new(position: Position.new('c1'), owner: @white))
+    @game_state.add_piece(Queen.new(position: Position.new('d1'), owner: @white))
+    @game_state.add_piece(King.new(position: Position.new('e1'), owner: @white))
+    @game_state.add_piece(Bishop.new(position: Position.new('f1'), owner: @white))
+    @game_state.add_piece(Knight.new(position: Position.new('g1'), owner: @white))
+    @game_state.add_piece(Rook.new(position: Position.new('h1'), owner: @white))
+
+    @game_state.add_piece(WhitePawn.new(position: Position.new('a2'), owner: @white))
+    @game_state.add_piece(WhitePawn.new(position: Position.new('b2'), owner: @white))
+    @game_state.add_piece(WhitePawn.new(position: Position.new('c2'), owner: @white))
+    @game_state.add_piece(WhitePawn.new(position: Position.new('d2'), owner: @white))
+    @game_state.add_piece(WhitePawn.new(position: Position.new('e2'), owner: @white))
+    @game_state.add_piece(WhitePawn.new(position: Position.new('f2'), owner: @white))
+    @game_state.add_piece(WhitePawn.new(position: Position.new('g2'), owner: @white))
+    @game_state.add_piece(WhitePawn.new(position: Position.new('h2'), owner: @white))
+
+    @game_state.add_piece(Rook.new(position: Position.new('a8'), owner: @black))
+    @game_state.add_piece(Knight.new(position: Position.new('b8'), owner: @black))
+    @game_state.add_piece(Bishop.new(position: Position.new('c8'), owner: @black))
+    @game_state.add_piece(Queen.new(position: Position.new('d8'), owner: @black))
+    @game_state.add_piece(King.new(position: Position.new('e8'), owner: @black))
+    @game_state.add_piece(Bishop.new(position: Position.new('f8'), owner: @black))
+    @game_state.add_piece(Knight.new(position: Position.new('g8'), owner: @black))
+    @game_state.add_piece(Rook.new(position: Position.new('h8'), owner: @black))
+
+    @game_state.add_piece(BlackPawn.new(position: Position.new('a7'), owner: @black))
+    @game_state.add_piece(BlackPawn.new(position: Position.new('b7'), owner: @black))
+    @game_state.add_piece(BlackPawn.new(position: Position.new('c7'), owner: @black))
+    @game_state.add_piece(BlackPawn.new(position: Position.new('d7'), owner: @black))
+    @game_state.add_piece(BlackPawn.new(position: Position.new('e7'), owner: @black))
+    @game_state.add_piece(BlackPawn.new(position: Position.new('f7'), owner: @black))
+    @game_state.add_piece(BlackPawn.new(position: Position.new('g7'), owner: @black))
+    @game_state.add_piece(BlackPawn.new(position: Position.new('h7'), owner: @black))
   end
 end
