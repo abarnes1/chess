35130652671bcf24f8eb1fcdbd2d57afee39c5b1
Aaron Barnes <aaron.barnes1@hotmail.com>
2021-10-28 18:45:47 -0500

    rework Castling to use CastlingRights and CastingPair classes

--- lib/actions/castling.rb
+++ lib/actions/castling.rb
@@ -17,54 +17,27 @@ class Castling < Action
     @partner_move_to = partner_move_to
   end
 
-  def self.create_for(piece, game_state)
-    return nil if game_state.nil? || !valid_initiator?(piece, game_state)
+  def self.create_for(king, game_state)
+    return nil if game_state.nil?
 
     actions = []
 
-    partners = valid_partners(piece, game_state)
+    active_pairs = game_state.castling_rights(king.owner)
 
-    partners.each do |partner|
-      destination = initiator_destination(piece, partner)
-      partner_destination = partner_destination(piece, partner)
+    active_pairs.each do |pair|
+      next unless valid_pair?(pair, game_state)
 
-      castling = new(piece, piece.position, destination, partner, partner_destination)
+      rook = game_state.select_position(pair.rook_position)
 
-      actions << castling unless illegal_movement?(piece, partner, game_state)
+      king_destination = king_destination(king, rook)
+      rook_destination = rook_destination(king, rook)
+
+      castling = new(king, king.position, king_destination, rook, rook_destination)
+
+      actions << castling unless illegal_movement?(king, rook, game_state)
     end
 
     actions
-  end
-
-  def self.valid_initiator?(initiator, game_state)
-    return false unless initiator.respond_to?(:initiates_castling?) && initiator.initiates_castling?
-
-    valid_initiator_position?(initiator.position) && !game_state.moved?(initiator)
-  end
-
-  def self.valid_initiator_position?(position)
-    valid_positions = [
-      Position.new('e1'),
-      Position.new('e8')
-    ]
-
-    valid_positions.include?(position)
-  end
-
-  def self.valid_partner?(initiator, partner, game_state)
-    return false unless partner.respond_to?(:castling_partner?) && partner.castling_partner?
-
-    valid_partner_position?(initiator.position, partner.position) && !game_state.moved?(partner)
-  end
-
-  def self.valid_partner_position?(initiating_position, partner_position)
-    if initiating_position == Position.new('e1')
-      [Position.new('a1'), Position.new('h1')].include?(partner_position)
-    elsif initiating_position == Position.new('e8')
-      [Position.new('a8'), Position.new('h8')].include?(partner_position)
-    else
-      false
-    end
   end
 
   def apply(_game_state)
@@ -90,53 +63,54 @@ class Castling < Action
   class << self
     private
 
-    def initiator_destination(initiator, partner)
-      x_direction = left_castle?(initiator, partner) ? -2 : 2
+    def valid_pair?(pair, game_state)
+      king = game_state.select_position(pair.king_position)
+      rook = game_state.select_position(pair.rook_position)
+
+      return false if king.nil? || !king.instance_of?(King)
+      return false if rook.nil? || !rook.instance_of?(Rook)
+      return false if king.owner != rook.owner
+
+      true
+    end
+
+    def king_destination(king, rook)
+      x_direction = left_castle?(king, rook) ? -2 : 2
       offset = Offset.new([x_direction, 0])
 
-      path_from_offset(initiator.position, offset)[0]
+      path_from_offset(king.position, offset)[0]
     end
 
-    def partner_destination(initiator, partner)
-      x_direction = left_castle?(initiator, partner) ? 3 : -2
+    def rook_destination(king, rook)
+      x_direction = left_castle?(king, rook) ? 3 : -2
 
       offset = Offset.new([x_direction, 0])
-      path_from_offset(partner.position, offset)[0]
+      path_from_offset(rook.position, offset)[0]
     end
 
-    def left_castle?(piece, partner)
-      piece.position.file > partner.position.file
+    def left_castle?(king, rook)
+      king.position.file > rook.position.file
     end
 
-    def illegal_movement?(checkable_piece, partner, game_state)
-      return true if path_blocked?(checkable_piece, partner, game_state)
-      return true if path_attackable?(checkable_piece, partner, game_state)
+    def illegal_movement?(king, rook, game_state)
+      return true if path_blocked?(king, rook, game_state)
+      return true if path_attackable?(king, rook, game_state)
 
       false
     end
 
-    def path_blocked?(initiator, partner, game_state)
-      path = linear_path_from_positions(initiator.position, partner.position)
+    def path_blocked?(king, rook, game_state)
+      path = linear_path_from_positions(king.position, rook.position)
 
       positions_to_check = path[1...-1]
       positions_to_check.any? { |position| game_state.occupied_at?(position) }
     end
 
-    def path_attackable?(initiator, partner, game_state)
-      destination = initiator_destination(initiator, partner)
-      path = linear_path_from_positions(initiator.position, destination)
+    def path_attackable?(king, rook, game_state)
+      destination = king_destination(king, rook)
+      path = linear_path_from_positions(king.position, destination)
 
-      path.any? { |position| game_state.attackable_by_enemy?(initiator.owner, position) }
-    end
-
-    def valid_partners(initiator, game_state)
-      return [] unless valid_initiator?(initiator, game_state)
-
-      partners = []
-      partners << game_state.select_position(Position.new("a#{initiator.position.rank}"))
-      partners << game_state.select_position(Position.new("h#{initiator.position.rank}"))
-
-      partners.compact.select { |partner| valid_partner?(initiator, partner, game_state) }
+      path.any? { |position| game_state.attackable_by_enemy?(king.owner, position) }
     end
   end
 end
