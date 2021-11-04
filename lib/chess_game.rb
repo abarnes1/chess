690429e509d234c6eb07583503fbeb51d6f896bd
Aaron Barnes <aaron.barnes1@hotmail.com>
2021-11-03 19:15:35 -0500

    figure out normal game loop

--- lib/chess_game.rb
+++ lib/chess_game.rb
@@ -3,50 +3,44 @@
 require_relative 'game_state'
 require_relative 'human_player'
 require_relative 'computer_player'
+require_relative 'display/chessboard'
 
 # Logic to setup, start, allow moves, and end a game of chess.
 class ChessGame
   def initialize
-    # require PGN fields in this order
-    @event = nil
-    @site = nil
-    @date = nil
-    @round = nil
     @white = 'white'
     @black = 'black'
-    @result = nil
 
-    # white turn + black turn
-    @rounds = nil
-
-    # optional PGN fields
-    @current_fen = nil
-
-    # classes necessary for game to run
     @game_state = nil
     @current_player = nil
     @winner = nil
+    @game_over = false
+
+    @chessboard = Chessboard.new
   end
 
   def play_game
     print_intro
     setup_game
-    puts @gameboard.to_s
+    puts @chessboard.to_s
 
     play_turns
+
     end_game
   end
 
   def setup_game
+    # new game or load here
     # create players
     @white = create_player('white', Colors::CYAN)
     @black = create_player('black', Colors::BRIGHT_MAGENTA)
     @current_player = @white
 
-    @game_state = GameState.standard_pieces(@white, @black)
+    @game_state = GameState.new(white: @white, black: @black)
+    standard_piece_setup
   end
 
-  def create_player(label)
+  def create_player(label, color)
     player_type = ''
 
     until %w[1 2].include?(player_type)
@@ -57,74 +51,71 @@ class ChessGame
     puts ''
 
     if player_type.eql?('1')
-      HumanPlayer.new(label)
+      HumanPlayer.new(label, color)
     else
-      ComputerPlayer.new(label)
+      ComputerPlayer.new(label, color)
     end
   end
 
   def play_turns
-    until game_over?
+    until @game_over
+      # system('clear')
+
+      @chessboard.reset_squares
+      @chessboard.display_pieces(@game_state.pieces)
+      puts @chessboard.display
+
       play_next_turn(@current_player)
-      puts @gameboard.to_s
 
-      @winner = @current_player if game_won?
+      @winner = @current_player if game_over?
 
-      switch_player if @winner.nil?
+      switch_player
     end
   end
 
   def play_next_turn(player)
-    # get legal moves
-    # legal_moves = @game_state.valid_moves(@current_player)
+    puts '-------- start of turn ---------'
+    moves = @game_state.legal_moves(@current_player)
 
-    # if game is forced draw then end (75 moves, 5 fold repitition)
-    # if player is checkmated or stalemated then end
-    # if player can request a draw, notify (50 moves, 3 fold repitition)
+    if game_over?(moves)
+      @winner = opposing_player if @game_state.in_check?(@current_player)
+      return
+    end
 
-    # otherwise, get valid player input and apply the selected move
+    chosen_move = player.choose_action(moves)
+
+    puts "#{@current_player.name} #{chosen_move}"
+    @game_state.apply_action(chosen_move)
+  end
+
+  def game_over?(legal_moves = nil)
+    return false if legal_moves.nil? || legal_moves.size.positive?
+
+    true
   end
 
   def player_input(player)
-    # column = nil
-
-    # until valid_move?(column)
-    #   if player.instance_of?(HumanPlayer)
-    #     print "Choose a column for #{player.name} #{player.token} : "
-    #     column = gets.chomp.to_i
-    #   elsif player.instance_of?(ComputerPlayer)
-    #     sleep(1)
-    #     column = player.choose_move(@gameboard.valid_moves)
-    #     puts "#{player.name} #{player.token}  chooses column ##{column}"
-    #   end
-    # end
-
-    # column
+    player.choose_action(actions)
   end
 
-  def game_won?
-
+  def end_game
+    puts 'game ended'
   end
 
   private
 
-  def to_pgn_format
-    raise NotImplementedError
-  end
-
-  def load_pgn
-    # some class to parse PGN and set up internal game state
-    raise NotImplementedError
-  end
-
   def switch_player
-    @current_player = @current_player == @white ? @black : @white
+    @current_player = opposing_player
+  end
+
+  def opposing_player
+    @current_player == @white ? @black : @white
   end
 
   def print_intro
     puts 'Play some chess.'
   end
-  
+
   def standard_piece_setup
     @game_state.add_piece(Rook.new(position: Position.new('a1'), owner: @white))
     @game_state.add_piece(Knight.new(position: Position.new('b1'), owner: @white))
