b17bf85baafe96c0f25a2be55ef225ca8fc2910a
Aaron Barnes <aaron.barnes1@hotmail.com>
2021-11-13 13:30:32 -0600

    update Move class tests with BoardData changes

--- spec/actions/move_spec.rb
+++ spec/actions/move_spec.rb
@@ -1,22 +1,24 @@
 require_relative '../../lib/actions/move'
 require_relative '../../lib/pieces/chesspiece'
+require_relative '../../lib/board_data'
 
 describe Move do
   describe '#apply' do
     let(:move_from) { Position.new('b2') }
     let(:move_to) { Position.new('b3') }
     let(:piece) { ChessPiece.new }
-    let(:game_state) { double('game_state') }
+    let(:board_data) { BoardData.new }
 
     subject(:apply_move) { described_class.new(piece, move_from, move_to)}
 
     before do
       piece.position = move_from
+      board_data.add_piece(piece)
     end
 
     it 'moves the piece to new position' do
       expect {
-        apply_move.apply(game_state)
+        apply_move.apply(board_data)
       }.to change { piece.position }.from(move_from).to(move_to)
     end
   end
@@ -25,17 +27,18 @@ describe Move do
     let(:move_from) { Position.new('b2') }
     let(:move_to) { Position.new('b3') }
     let(:piece) { ChessPiece.new }
-    let(:game_state) { double('game_state') }
+    let(:board_data) { BoardData.new }
 
     subject(:apply_move) { described_class.new(piece, move_from, move_to)}
 
     before do
       piece.position = move_to
+      board_data.add_piece(piece)
     end
 
     it 'moves the piece back to original position' do
       expect {
-        apply_move.undo(game_state)
+        apply_move.undo(board_data)
       }.to change { piece.position }.from(move_to).to(move_from)
     end
   end
