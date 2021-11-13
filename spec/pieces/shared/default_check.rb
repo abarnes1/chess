710db490157efd76e9abf7182159e5e8ded3a3e7
Aaron Barnes <aaron.barnes1@hotmail.com>
2021-11-12 22:42:44 -0600

    remove shared tests for ChessPiece methods that are gone

--- spec/pieces/shared/default_check.rb
+++
@@ -1,8 +0,0 @@
-RSpec.shared_examples 'default check behavior' do
-  context '#can_be_checked?' do
-    it 'returns false' do
-      actual = subject.can_be_checked?
-      expect(actual).to be false
-    end
-  end
-end
